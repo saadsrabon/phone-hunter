01e2598f0d241e6331e4e10e635b07b46d0d96e4
Saad Srabon <saadsrabon2@gmail.com>
2022-09-01 19:14:13 +0600

    final update

--- app.js
+++ app.js
@@ -17,19 +17,26 @@
 
 // 4 Show and hide loading spinner while loading API data
 
-const loadPhones = async(search) => {
+const loadPhones = async(search , dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${search}`
     const res = await fetch(url);
     const data = await res.json();
-    display(data.data)
+    display(data.data ,dataLimit)
 
 }
 // Load Dta
- const display= (data) =>{
+ const display= (data ,dataLimit) =>{
       const phoneRow = document.getElementById('phone-row');
       phoneRow.textContent =""
-
-    //   Checking Conditon for array length for saying sorry
+      const showAll = document.getElementById('show-all')
+      if(dataLimit && data.length >10){
+        data = data.slice(0,10);
+        showAll.classList.remove('d-none')
+      }else{
+        
+        showAll.classList.add('d-none')
+      }
+    //   Checking Conditon for array length for saying sorry-
       if(data.length === 0){
         const nothing = document.getElementById('nothing-text')
         nothing.classList.remove('d-none')
@@ -39,20 +46,24 @@ const loadPhones = async(search) => {
       }
 
     //   Slice data array for reduce display file
-      data =data.slice(0,6)
+      // data =data.slice(0,6)
 
     //   itterating for getting each phone and innerhtml
       data.forEach(element => {
-        console.log(element)
+       
          const phoneCol = document.createElement('div');
          phoneCol.classList.add('col');
          phoneCol.innerHTML = `<div class="card px-4 py-4">
-         <img src="${element.image}" class="card-img-top w-50 ms-5 " alt="...">
+         <img src="${element.image}" class="card-img-top w-50  " alt="...">
          <div class="card-body">
            <h5 class="card-title">${element.phone_name}</h5>
-           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+           <p class="card-textt">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
          </div>
+         <button onclick ="phoneDetails('${element.slug}')" type="button" class="btn btn-primary w-50 btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+Phone Details
+</button>
        </div>`
+       
 //   append the child to the parent div
        phoneRow.appendChild(phoneCol)
       });
@@ -61,13 +72,14 @@ const loadPhones = async(search) => {
 
 //  Input Button Event Handler for pass searh text
 document.getElementById('search-btn').addEventListener('click' ,()=> {
-  loadergif(true)
-    const input = document.getElementById('input-field');
-    const searchText = input.value;
-    loadPhones(searchText);
-    input.value ="";
+  processSearch(10)
 })
 
+// Show all btn functionality
+
+document.getElementById('show-all-btn').addEventListener('click' , ()=>{
+    processSearch()
+})
 
 const loadergif =(isLoading) =>{
 const loader = document.getElementById('loader')
@@ -77,3 +89,48 @@ const loader = document.getElementById('loader')
     loader.classList.add('d-none')
   }
 }
+
+// input addEventListener For enter
+document.getElementById('input-field').addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    processSearch(10);
+  }
+});
+
+
+// Search Function
+const processSearch =(datalimit) =>{
+  loadergif(true)
+  const input = document.getElementById('input-field');
+  const searchText = input.value;
+  loadPhones(searchText ,datalimit);
+  input.value ="";
+}
+
+// phone details Function
+const phoneDetails = (id) =>{
+   const url =`https://openapi.programming-hero.com/api/phone/${id}` 
+   fetch(url)
+   .then(res => res.json())
+   .then(data => showPhoneDetails(data.data))
+
+}
+// Phone Details from Api To ui
+const showPhoneDetails =(data)=>{
+console.log(data)
+    const modalTitle =document.getElementById('modals-title');
+    const modalBody = document.getElementById('modal-body')
+    modalTitle.innerText =data.name;
+    modalBody.innerHTML =`
+    <p >Brand:${data.brand}</p>
+    <p >Release Date:${data.releaseDate}</p>
+    <p >Storage:${data.mainFeatures.storage}</p>
+    <p >Display:${data.mainFeatures.displaySize}</p>
+    <p >Chipset:${data.mainFeatures.chipSet}</p>
+    <p >Memory:${data.mainFeatures.memory}</p>
+    `
+}
+// Always load the apple phones first 
+loadPhones('apple')
+
+
---
+++ e
@@ -0,0 +1,7 @@
+brand: "Apple"
+image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg"
+mainFeatures: {storage: '128GB/256GB/512GB storage, no card slot', displaySize: '5.4 inches, 71.9 cm2 (~85.1% screen-to-body ratio)', chipSet: 'Apple A15 Bionic (5 nm)', memory: '128GB 4GB RAM, 256GB 4GB RAM, 512GB 4GB RAM', sensors: Array(6)}
+name: "iPhone 13 mini"
+releaseDate: "Released 2021, September 24"
+slug: "apple_iphone_13_mini-11104"
+[[Prototype]]: Object
